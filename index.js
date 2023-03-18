e4b5209e49191ec4b79177808c1965b86ac11d4c
diaken2 <107970623+diaken2@users.noreply.github.com>
2023-03-19 00:01:52 +0500

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ app.use("/api", routes)
 app.use(express.urlencoded({extended:false}))
 app.use('/images',express.static(path.join(__dirname,"images")))
 app.use(express.static(path.join(__dirname, 'build')));
+app.use(function(req, res, next) {
+
+
+  res.header("Access-Control-Allow-Origin","*"); // update to match the domain you will make the request from
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+  
+
+  
+  });
 const PORT=process.env.PORT || 5000
 const start=async()=>{
     try{
